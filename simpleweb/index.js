7e3e78a3d7213dbb8c79ea6461c6014336faa197
guohaoyu110 <guohaoyu110@gmail.com>
2020-05-15 16:17:10 -0700

    fix error and rebuild simpleweb image

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
 const app = express();
+// if i want to make the modification in the container, 
+// we have to rebuild the container
+
 
 app.get('/', (req, res) => {
-    res.send('Hi there');
+    res.send('Bye there, how are you doing?');
 });
 
 app.listen(8080 , () => {
     console.log('Listening on port 8080');
     
 });
+
+// 如果在index.js里面改动过了，一定要重新build 这个image
 
